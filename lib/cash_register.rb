ff567b1980af969fb320ac46a26ee65701e05398
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-28 22:49:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   
   def apply_discount
     @total = total - (total * discount/100)  
-    puts 
+  if discount != 0  puts 
     "After the discount, the total comes to $#{ @total}."
   end
 end
