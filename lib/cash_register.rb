327f8d6197b6ce738d401b7a251064521d99b714
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-28 22:20:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+class CashRegister
+  attr_reader :discount
+  attr_accessor :total
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
 
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+  end
+  
+  def apply_discount
+    @total = total - (total * discount/100)  
+    puts 
+    "After the discount, the total comes to $#{ @total}."
+  end
+end
